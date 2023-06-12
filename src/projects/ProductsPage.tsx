d8aa090505934641eb7e29ea5178fc65687b5bba
BmDevelopers001 <bmsavaliya001@gmail.com>
2023-06-12 17:52:22 +0530

    Reusable Components

---
+++ src/projects/ProductsPage.tsx
@@ -0,0 +1,17 @@
+import { stringify } from "querystring";
+import React from "react";
+import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from "./ProjectList";
+
+function ProjectsPage(){
+    return(
+        <>
+            <h1>
+                Projects
+            </h1>
+            <ProjectList projects={MOCK_PROJECTS} />
+        </>
+    )
+}
+
+export default ProjectsPage;
